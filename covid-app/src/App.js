c9c87ed1efd82edc1f5a6045c1a3efd925201596
Christian Montero <cmontero@bu.edu>
2020-04-08 19:54:12 -0400

    minor

--- covid-app/src/App.js
+++ covid-app/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
+import { Container, Row, Navbar, Nav } from 'react-bootstrap';
 import Landing from './components/Landing';
 import SearchDetails from './components/SearchDetails'
+import person from './img/person.png'
 import './App.css';
 
 
 function App() {
   return (
-    <Router> 
-       <Container className="App" fluid>
-       <header className = 'App-header'>
-          <a href = '/'>
-            <h1> COVID SITE </h1>
-          </a>
-        </header>
+    <Router>
+      <Container className = "App" fluid>
+        <Row>
+        <Navbar className = 'App-nav' fixed = 'top'>
+          <Navbar.Brand href = '/' > COVID-19 Info Hub </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+             <Nav.Link href = '/account'> Account </Nav.Link>
+             <Nav.Link href = '/account/messages'> Messages </Nav.Link>
+             <Nav.Link href = '/logout'> Logout </Nav.Link>
+          </Navbar.Collapse>
+        </Navbar>
+        </Row>
+        <Row>
         <Switch>
-          <Route exact path = '/' component = {Landing} /> 
-          <Route exact path = '/searchDetails' component = {SearchDetails} /> 
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/searchDetails' component={SearchDetails} />
         </Switch>
+        </Row>      
       </Container>
     </Router>
   );
