e836bdfadc8db547032289171a15bbb88d3c099a
Christian Montero <cmontero@bu.edu>
2020-04-26 21:40:05 -0400

    terminate seed

--- covid-app/config/mongoConnection.js
+++ covid-app/config/mongoConnection.js
@@ -14,8 +14,10 @@ let _db = undefined;
 
 module.exports = async () => {
     if (!_connection) {
-        _connection = await MongoClient.connect(mongoConfig.serverUrl, { useNewUrlParser: true, useUnifiedTopology: true});
+        _connection = await MongoClient.connect(mongoConfig.serverUrl, { useUnifiedTopology: true });
         _db = await _connection.db(mongoConfig.database);
     };
+
     return _db;
 };
+
