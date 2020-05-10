6995b25a26d90efec8b0b225b49fe4d0e4eac8e8
Christian Montero <cmontero@bu.edu>
2020-05-10 09:13:35 -0400

    404 route

---
+++ covid-app/client/src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+
+const NotFound = () => {
+    return (
+        <Container className='main' fluid >
+            <div>
+                <h1> ERROR 404: Page Not Found </h1>
+            </div>
+        </Container>
+    );
+};
+
+export default NotFound;
