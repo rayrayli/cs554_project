6f2478f40cc932531e714976c7ea8ae7a6b16bef
Christian Montero <cmontero@bu.edu>
2020-05-14 15:49:07 -0400

    .

---
+++ covid-app/client/src/components/Inbox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Modal Used to Confirm Addresses for ALL Users 
+const Inbox = (props) => {
+    return (
+        <Container className='main' fluid>
+            <h1> MESSAGES</h1>
+        </Container>
+
+    );
+};
+
+export default Inbox;
