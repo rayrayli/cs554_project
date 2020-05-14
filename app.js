f89dc06c4f4d5de517bc97a49ed4a98446afde58
Christian Montero <cmontero@bu.edu>
2020-05-14 18:02:05 -0400

    .

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('client/build'));
+// app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
