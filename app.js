77cb1313c5e8af2b1c62c749906b8110b6485cab
Christian Montero <cmontero@bu.edu>
2020-05-14 18:01:04 -0400

    .

--- app.js
+++ app.js
@@ -35,13 +35,11 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 /*
 ######################### ROUTES #########################
